4555fbb232626a0ee0b1da696fe86e4fcb625cb4
Catherine Sweeney <catherine.sweeney@neurohero.com>
2012-12-14 03:05:39 -0800

    Loaded from backup

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NHMatch
+//
+//  Created by Catherine Sweeney on 10/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
